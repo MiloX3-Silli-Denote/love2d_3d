08a4943f3ac6ad77bbc05501bf76dc63346d4992
Milo:3 Silli Denote <71358745+MiloX3-Silli-Denote@users.noreply.github.com>
2025-05-29 10:08:53 -0400

    Update init.lua

--- threedee/init.lua
+++ threedee/init.lua
@@ -10,6 +10,7 @@ function Threedee.init()
   local Model = require(path .. "/model");
   local Mesh = require(path .. "/mesh");
   local Parser = require(path .. "/parser");
+  local World = require(path .. "/world");
 
   Threedee.newMatrx = Matrix.new;
   Threedee.newCamera = Camera.new;
@@ -17,15 +18,39 @@ function Threedee.init()
   Threedee.newModel = Model.new;
   Threedee.newMesh = Mesh.new;
   Threedee.loadModel = Parser.loadModel;
+  Threedee.newWorld = World.new;
 
   self.activeCamera = Camera.new(); -- create a new camera as the main camera
+  self.activeWorld = World.new(); -- create a new world as the main world
 end
 
 function Threedee.setActiveCamera(cam)
   self.activeCamera = cam;
 end
+function Threedee.setActiveWorld(world)
+  self.activeWorld = world;
+end
 function Threedee.getCamera()
   return self.activeCamera;
+end
+function Threedee.getWorld()
+  return self.activeWorld;
+end
+
+function Threedee.loadPrefab(...)
+  self.activeWorld:loadPrefab(...);
+end
+function Threedee.addToWorld(...)
+  self.activeWorld:addToWorld(...);
+end
+function Threedee.instantiate(...)
+  self.activeWorld:instantiate(...);
+end
+function Threedee.update(...)
+  self.activeWorld:update(...);
+end
+function Threedee.draw()
+  self.activeWorld:draw();
 end
 
 function Threedee.newShader(pixelCode, vertexCode)
